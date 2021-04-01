1e2254a6b993237df8ee40d1e0a0a7afe9ba8d87
Edgar Mejia <edgarinhm@hotmail.com>
2021-04-01 11:12:59 -0500

    java validator

---
+++ usermanageer/src/main/java/cds/poweredge/usermanageer/UsermanageerApplication.java
@@ -0,0 +1,13 @@
+package cds.poweredge.usermanageer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanageerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanageerApplication.class, args);
+	}
+
+}
